f7f2739135ef1112d64e4673c5cdaea9efa4ba5e
Kevinlega <kevinleglez@gmail.com>
2018-02-06 12:33:16 -0400

    Working Parser and dbg builder

--- Parser.py
+++ Parser.py
@@ -16,7 +16,7 @@ import os
 import sys
 import fileinput
 import argparse
-import gfapy
+
 
 
 # Make output directory
@@ -30,32 +30,6 @@ if not os.path.exists("Parser_Output"):
 #                                          #
 ############################################
 
-
-# def coverageSegmentF(C,kmerA,kmerB,Db,k):
-# 	global g
-# 	kA,kB = kmerA,kmerB
-
-# 	kA = kA.split("A:")
-# 	kA = kA[1]
-# 	kA = kA.split(',B:')
-# 	kA[1] = kA[1].split(")")[0]
-
-#  	kB = kB.split("A:")
-# 	kB = kB[1]
-# 	kB = kB.split(',B:')
-# 	kB[1] = kB[1].split(")")[0]
-
-# 	coverageA = abs(int(kA[0]) - int(kA[1])) 
-# 	coverageB = abs(int(kB[0]) - int(kB[1]))
-# 	DifEx = (coverageA + coverageB)/2
-
-# 	if DifEx < C:
-# 		return False
-# 	else:
-# 		a = ("L\t%s\t+\t%s\t%s\t%dM\tKC:i:%d"%(kmerA,kmerB,dB,k-1,int(DifEx)))
-# 		return a
-
-
 def positive(C):
 	C = int(C)
 	if C <= 0:
@@ -65,43 +39,31 @@ def positive(C):
 
 
 def main():
-    global C,g,args
+    global C,args
     #maybe clean all the unnatached segments
-    output = gfapy.Gfa()
-    output.add_line(g.header)
-    all_segments = [output.add_line(str(line)) for line in g.segments]
-    links = [str(line) for line in g.edges]
-
-    for x in range(len(links)-1):
-        link = links[x]
-        kmerA = link.split("\t")
-        k = kmerA[5]
-        dB = kmerA[4]
-        kmerB = kmerA[3]
-        kmerA = kmerA[1]
-        kA,kB = kmerA,kmerB
-        kA = kA.split("A:")
-        kA = kA[1]
-        kA = kA.split(',B:')
-        kA[1] = kA[1].split(")")[0]
-        kB = kB.split("A:")
-        kB = kB[1]
-        kB = kB.split(',B:')
-        kB[1] = kB[1].split(")")[0]
-        coverageA = abs(int(kA[0]) - int(kA[1])) 
-        coverageB = abs(int(kB[0]) - int(kB[1]))
-        DifEx = (coverageA + coverageB)/2
-        if DifEx < C:
-            pass
-        else:
-            output.add_line("L\t%s\t+\t%s\t%s\t%s\tKC:i:%d"%(kmerA,kmerB,dB,k,int(DifEx)))
-
     filename = os.path.join("Parser_Output",args.output)
-    output.to_file(filename)
-
-
-
-
+    with open(filename,'w') as output:
+        with open(args.f,'r') as file:
+            for line in file:
+                line_split = line.split()
+                if line_split[0] == 'S':
+                    output.write(line)
+                elif line_split[0] == 'L':
+                    kA = line_split[1].split("A:")
+                    kA = kA[1]
+                    kA = kA.split(',B:')
+                    kA[1] = kA[1].split(")")[0]
+                    kB = line_split[3].split("A:")
+                    kB = kB[1]
+                    kB = kB.split(',B:')
+                    kB[1] = kB[1].split(")")[0]
+                    coverageA = abs(int(kA[0]) - int(kA[1])) 
+                    coverageB = abs(int(kB[0]) - int(kB[1]))
+                    DifEx = (coverageA + coverageB)/2
+                    if DifEx >= C:
+                        output.write("L\t%s\t%s\t%s\t%s\t%s\tKC:i:%d\n"%(line_split[1],line_split[2],line_split[3],line_split[4],line_split[5],int(DifEx)))
+                elif line_split[0] == 'H':
+                    output.write(line)
 
 
 parser = argparse.ArgumentParser(description="Creates a GFA file with one or two organisms given a kmer size. \
@@ -110,7 +72,6 @@ parser.add_argument("-f", required=True, help="the output file of dbg.py")
 parser.add_argument("-output",required=False,default='output.gfa',help="Output GFA file name")
 parser.add_argument("-C", required=True,type=positive,help="Eliminates links with Diferential Expresion below coverage")
 args = parser.parse_args()
-g = gfapy.Gfa.from_file(args.f) 
 C = args.C
 # To add more organisms add this parser.add_argument("-B", nargs='+', required=True, help="Organism_B_files")
 # change the name and do another call to build and do multiple merge_dicts calls
